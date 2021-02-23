e4cd64aa558ea15c3b20c31cb26932e4a17c0ee9
Brendan Dagys <brendandagys@gmail.com>
2021-02-23 18:38:50 -0500

    Generate a JSON Web Token

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -12,20 +13,22 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+
 // Middleware example
-app.use((req, res, next) => {
-  console.log(req.originalUrl)
-  next()
-})
+// app.use((req, res, next) => {
+//   console.log(req.originalUrl)
+//   next()
+// })
 
 app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
-
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
