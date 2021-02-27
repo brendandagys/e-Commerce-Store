eebd3f9ad936329a9ecc5b604cd6e2fc169f6df0
Brendan Dagys <brendandagys@gmail.com>
2021-02-27 11:20:42 -0500

    Admin Middleware & Get Users Endpoint

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -30,4 +30,14 @@ const protect = asyncHandler(async (req, res, next) => {
   }
 })
 
-export { protect }
+const admin = (req, res, next) => {
+  // Middleware functions take these 3 arguments
+  if (req.user && req.user.isAdmin) {
+    next()
+  } else {
+    res.status(401) // Not authorized
+    throw new Error('Not authorized as an admin')
+  }
+}
+
+export { protect, admin }
