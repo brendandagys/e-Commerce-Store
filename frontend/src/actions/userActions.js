088555352edb471a65f0dd8801b8fc239d6acc30
Brendan Dagys <brendandagys@gmail.com>
2021-02-28 10:17:41 -0500

    Navbar Bug Fix

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -152,6 +152,9 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
     const { data } = await axios.put('/api/users/profile', user, config)
 
     dispatch({ type: USER_UPDATE_PROFILE_SUCCESS, payload: data })
+    dispatch({ type: USER_LOGIN_SUCCESS, payload: data }) // To fix Navbar bug
+
+    localStorage.setItem('userInfo', JSON.stringify(data))
   } catch (error) {
     dispatch({
       type: USER_UPDATE_PROFILE_FAIL,
