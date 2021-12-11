7b43379ed94c186600b828c4b15e7bc1aec150b0
Brendan Dagys <brendandagys@gmail.com>
2021-12-11 18:28:16 -0500

    Try different `dotenv` configuration

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,10 @@ import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
 
-// dotenv.config()
-dotenv.config({ path: __dirname + (process.env.ENVPATH ?? '/.env') })
+dotenv.config()
+// if (process.env.NODE_ENV === 'development') {
+// dotenv.config({ path: __dirname + (process.env.ENVPATH ?? '/.env') })
+// }
 
 connectDB()
 
